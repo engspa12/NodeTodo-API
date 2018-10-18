9e771d9822c949fb7e5f120757c7407f6fca2041
Dani <arturo_lpc@hotmail.com>
2018-10-18 18:41:33 -0500

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -27,36 +27,4 @@ app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
-
-// var newUser = new User({
-//   email: '    johnd23g@gmail.com     '
-// });
-//
-// newUser.save().then((doc) => {
-//   console.log('Saved user\n', doc);
-// },(e) => {
-//   console.log('Unable to save user', e);
-// });
-
-
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// },(e) => {
-//   console.log('Unable to save todo');
-// });
-
-// var otherTodo = new Todo({
-//   text: '  Other Edit  ',
-//   // completed: false,
-//   // completedAt: 1345
-// });
-//
-// otherTodo.save().then((doc) => {
-//   console.log('Saved todo\n', doc);
-// }, (e) => {
-//   console.log('Unable to save',e);
-// });
+module.exports = {app};
