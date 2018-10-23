770463da777b63cb0e76dd210ba1636f81fca33d
Dani <arturo_lpc@hotmail.com>
2018-10-22 19:02:21 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,11 @@ var mongoose = require('mongoose');
 //{ useNewUrlParser: true }
 mongoose.Promise = global.Promise;
 mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
 
 module.exports = {
   mongoose: mongoose
 };
+
+//process.env.NODE_ENV === 'production' -----> Heroku
